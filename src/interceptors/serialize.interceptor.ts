8cad29e905d0c125cd2c493f329fcc3317176840
Marc-Antoine Duhaime <ma@duhaime.ca>
2022-07-04 12:30:23 -0400

    Custom Interceptor to exclude data from response

---
+++ src/interceptors/serialize.interceptor.ts
@@ -0,0 +1,24 @@
+import { UseInterceptors, NestInterceptor, ExecutionContext, CallHandler } from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { plainToInstance } from 'class-transformer';
+
+// Decorator
+export function Serialize(dto: any) {
+  return UseInterceptors(new SerializeInterceptor(dto));
+}
+
+// Interceptor
+export class SerializeInterceptor implements NestInterceptor {
+  constructor(private dto: any) {}
+
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    return next.handle().pipe(
+      map((data: any) => {
+        return plainToInstance(this.dto, data, {
+          excludeExtraneousValues: true,
+        });
+      }),
+    );
+  }
+}
